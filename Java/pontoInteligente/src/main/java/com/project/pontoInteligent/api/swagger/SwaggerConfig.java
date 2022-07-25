56c506963a6623bdc1b5c3c674407ce74426373d
Miguel Angelo <miguel.angelo.felix96@gmail.com>
2022-07-25 17:52:10 -0300

    .

--- Java/pontoInteligente/src/main/java/com/project/pontoInteligent/api/swagger/SwaggerConfig.java
+++
@@ -1,2 +0,0 @@
-package com.project.pontoInteligent.api.swagger;public class SwaggerConfig {
-}
--- Java/pontoInteligente/src/main/java/com/project/pontoInteligente/api/enums/PerfilEnum.java
+++
@@ -1,6 +0,0 @@
-package com.project.pontoInteligente.api.enums;
-
-public enum PerfilEnum {
-	ROLE_ADMIN,
-	ROLE_USUARIO;
-}
