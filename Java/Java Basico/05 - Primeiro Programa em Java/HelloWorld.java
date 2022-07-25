13134f20b844e8df1b5a9941f2dbbd89a9206475
Miguel Angelo <miguel.angelo.felix96@gmail.com>
2022-07-25 18:16:40 -0300

    Java Basico: 25.07.2022

---
+++ Java/Java Basico/05 - Primeiro Programa em Java/HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    // Compilar Aplicativo - javac HelloWorld.java
+    // Executar Aplicativo - java HelloWorld
+}
