ce3bf5196387350494bdd83811d43008d82784ff
Miguel Angelo <miguel.angelo.felix96@gmail.com>
2022-11-10 15:52:01 -0300

    .

--- Java/Java Basico/src/O25_Orientacao_Objetos_Metodos/Carro.java
+++ Java/Java Basico/src/O25_Orientacao_Objetos_Metodos/Carro.java
@@ -8,7 +8,42 @@ public class Carro {
 	double capCombustivel;
 	double consumoCombustivel;
 	
+	
+	
+	Carro(String marca, String modelo, int numPassageiros, double capCombustivel, double consumoCombustivel) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.numPassageiros = numPassageiros;
+		this.capCombustivel = capCombustivel;
+		this.consumoCombustivel = consumoCombustivel;
+	}
+	
+	Carro() {
+		System.out.println("Classe Carro foi instanciada");
+		numPassageiros = 4;
+	}
+	
+	Carro(String marca, String modelo) {
+		this("Fiat", "Ducato", 7, 10, 2);
+	}
+	
+	Carro(String marca) {
+		this.marca = marca;
+	}
+
 	void exibirAutonomia() {
 		System.out.println("A autonomia do carro é: " + capCombustivel * consumoCombustivel + " km");
+	}
+	
+	double obterAutonomia() {
+		return capCombustivel * consumoCombustivel;
+	}
+	
+	private double divideKMPorConsumoCOmbustivel(double km) {
+		return km/consumoCombustivel;
+	}
+	
+	double calcularCombustivel(double km) {
+		return this.divideKMPorConsumoCOmbustivel(km);
 	}
 }
--- Java/Java Basico/src/O25_Orientacao_Objetos_Metodos/main.java
+++ Java/Java Basico/src/O25_Orientacao_Objetos_Metodos/main.java
@@ -6,15 +6,26 @@ public class main {
 		Carro van = new Carro();
 		van.marca = "Fiat";
 		van.modelo = "Ducato";
-		van.numPassageiros = 10;
+		//van.numPassageiros = 10;
 		van.capCombustivel = 100;
 		van.consumoCombustivel = 0.2;
 
-		System.out.println(van.marca);
-		System.out.println(van.modelo);
-		System.out.println();
+//		System.out.println(van.marca);
+//		System.out.println(van.modelo);
+//		System.out.println();
+//		
+//		van.exibirAutonomia();
+//		System.out.println("A autonomia do carro é: " + van.obterAutonomia()  + " km");
+//		System.out.println();
+//		
+//		double qtdCombustivel = van.calcularCombustivel(12);
+//		System.out.println("qtdCombustivel: " + qtdCombustivel);
 		
-		van.exibirAutonomia();
+		System.out.println("numPassageiros: " + van.numPassageiros );
+		
+		Carro van2 = new Carro("BMW");
+		System.out.println("marca: " + van2.marca );
+		
 	}
 
 }
