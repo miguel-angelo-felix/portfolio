6b64177aa4bff2e66e9fdc7f475d33ed7a2c186e
Miguel Angelo <miguel.angelo.felix96@gmail.com>
2023-02-23 15:24:04 -0300

    micro frontend

---
+++ Angular/micro-frontend/app-master/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app-master';
+
+  ngOnInit(){
+    const scriptExtrato = document.createElement('script');
+    scriptExtrato.src = 'http://localhost:8080/extrato/main-es2022.js';
+    document.body.appendChild(scriptExtrato);
+
+    const scriptPagamento = document.createElement('script');
+    scriptPagamento.src = 'http://localhost:8080/pagamento/main-es2022.js';
+    document.body.appendChild(scriptPagamento);
+  }
+}
---
+++ Angular/micro-frontend/app-master/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
